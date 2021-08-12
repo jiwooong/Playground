637901fa438085ea986fb852a375b55dd9e185a5
jiwoong <jiwoong@jiwoongui-MacBookPro.local>
2021-08-12 17:13:16 +0900

    ConsolesView (SwiftUI) 파일 추가

---
+++ Supporting Views/ConsolesView.swift
@@ -0,0 +1,20 @@
+//
+//  ConsolesView.swift
+//  Playground
+//
+//  Created by jiwoong on 2021/08/12.
+//
+
+import SwiftUI
+
+struct ConsolesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ConsolesView_Previews: PreviewProvider {
+    static var previews: some View {
+        ConsolesView()
+    }
+}
--- ViewControllers/Detail ViewControllers/NConsoleViewController.swift
+++ ViewControllers/Detail ViewControllers/NConsoleViewController.swift
@@ -6,42 +6,44 @@
 //
 
 import UIKit
+import SwiftUI
 
 class NConsoleViewController: UIViewController {
     
     // MARK: - Var
     
-    @IBOutlet weak var stackView: UIStackView!
-    @IBOutlet weak var lblTitle: UILabel!
-    
-//    var nConsoleImages = []
+    let consolesView = UIHostingController(rootView: ConsolesView())
     
     // MARK: - Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        
-        setStackView()
+        self.addChild(consolesView)
+        self.view.addSubview(consolesView.view)
+        setConstraints()
     }
     
     // MARK: - Set
     
-    private func setVar() {
-        lblTitle.text = "Here are Nintendo's Consoles"
+    private func setConstraints() {
+        consolesView.view.translatesAutoresizingMaskIntoConstraints = false
+        consolesView.view.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
+        consolesView.view.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+        consolesView.view.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
+        consolesView.view.rightAnchor.constraint(equalTo: self.view.rightAnchor).isActive = true
     }
+//    private func setVar() {
+//        lblTitle.text = "Here are Nintendo's Consoles"
+//    }
     
-    private func setStackView() {
-        for _ in 0...10 {
-            if let consoleImageView = Bundle.main.loadNibNamed("ConsoleImageView", owner: nil, options: nil)!.first as? ConsoleImageView {
-                consoleImageView.translatesAutoresizingMaskIntoConstraints = false
-                consoleImageView.widthAnchor.constraint(equalToConstant: stackView.frame.height).isActive = true
-                consoleImageView.ivConsole.image = UIImage(named: "mario-detail.jpg")
-                stackView.addArrangedSubview(consoleImageView)
-            }
-        }
-    }
+//    private func setStackView() {
+//        for _ in 0...10 {
+//            if let consoleImageView = Bundle.main.loadNibNamed("ConsoleImageView", owner: nil, options: nil)!.first as? ConsoleImageView {
+//                consoleImageView.translatesAutoresizingMaskIntoConstraints = false
+//                consoleImageView.widthAnchor.constraint(equalToConstant: stackView.frame.height).isActive = true
+//                consoleImageView.ivConsole.image = UIImage(named: "mario-detail.jpg")
+//                stackView.addArrangedSubview(consoleImageView)
+//            }
+//        }
+//    }
 }
