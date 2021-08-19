024cc21c01ec41c3e22826e0030af4853a2446cd
jiwoong <>
2021-08-19 09:29:22 +0900

    ConsoleItem 파일 추가

---
+++ Supporting Views/SwiftUI Views/ConsoleItem.swift
@@ -0,0 +1,30 @@
+//
+//  ConsoleItem.swift
+//  Playground
+//
+//  Created by jiwoong on 2021/08/19.
+//
+
+import SwiftUI
+
+struct ConsoleItem: View {
+    var viewModel: NintendoViewModel
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            viewModel.consoleImages[0]
+                .resizable()
+                .frame(width: 155, height: 155)
+                .cornerRadius(5)
+            Text(viewModel.consoleTitles[1])
+                .font(.caption)
+        }
+        .padding(.leading, 15)
+    }
+}
+
+//struct ConsoleItem_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ConsoleItem()
+//    }
+//}
