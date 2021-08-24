67c4007ad2d0bf0871e5fb0675c46cbd0b65262f
jiwoong <>
2021-08-24 10:07:25 +0900

    ConsoleView  수정

--- Supporting Views/SwiftUI Views/ConsolesView.swift
+++ Supporting Views/SwiftUI Views/ConsolesView.swift
@@ -14,35 +14,28 @@ struct ConsolesView: View {
         NavigationView {
             List {
                 VStack {
+                    Image(uiImage: UIImage(named: "mario-detail.jpg")!)
+                        .resizable()
+                        .scaledToFill()
+                        .frame(height: 248)
+                        .clipped()
+                }
+                
+                VStack(alignment: .leading) {
                     Text(viewModel.consoleTitles[0])
                         .font(.headline)
                         .padding(.leading, 15)
                         .padding(.top, 5)
                     
-                    Image(uiImage: UIImage(named: "mario-detail.jpg")!)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(height: 80)
-                        .padding()
-                }
-                
-                ScrollView(.horizontal, showsIndicators: false) {
-                    HStack(alignment: .top, spacing: 0) {
-                        ForEach(viewModel.consoleTitles.indices) { index in
-                            ConsoleItem(viewModel: viewModel)
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack(alignment: .top, spacing: 0) {
+                            ForEach(viewModel.consoleTitles.indices) { index in
+                                ConsoleItem(viewModel: viewModel)
+                            }
                         }
                     }
+                    .frame(height: 185)
                 }
-                .frame(height: 185)
-                
-                ScrollView(.horizontal, showsIndicators: false) {
-                    HStack(alignment: .top, spacing: 0) {
-                        ForEach(viewModel.consoleTitles.indices) { index in
-                            ConsoleItem(viewModel: viewModel)
-                        }
-                    }
-                }
-                .frame(height: 185)
             }
             .navigationTitle("Mario")
         }
